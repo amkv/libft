86e527ec858d0cd084853abc585f949084023744
Artem Kalmykov <kalmykov.artem@gmail.com>
2016-10-01 17:29:48 -0700

    last changes for today

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -28,33 +28,31 @@ free
 
 #include "libft.h"
 
-int		main(void)
-{
-	char	*s;
-	char	c;
-	char	**str;
+// int		main(void)
+// {
+// 	char	*s;
+// 	char	c;
+// 	char	**str;
 
-	s = "md td ";
-	c = ' ';
-	//printf("str: %s\n", s);
-	str = ft_strsplit(s, c);
-	while (**str)
-	{
-		printf("%s\n", *str);
-		str++;
-	}
-	return (0);
-}
+// 	s = "md td ";
+// 	c = ' ';
+// 	//printf("str: %s\n", s);
+// 	str = ft_strsplit(s, c);
+// 	while (**str)
+// 	{
+// 		printf("result:  %s\n", *str);
+// 		str++;
+// 	}
+// 	return (0);
+// }
 
 char	**ft_strsplit(char const *s, char c)
 {
-	// char	*beg;
-	// char	*end;
-	//size_t	len;
+	char	*beg;
+	char	*end;
 	char	**str;
-	//char	*ptr;
 	size_t	index;
-	//char	*temp;
+	char	*temp;
 
 	if (s)
 	{
@@ -66,44 +64,37 @@ char	**ft_strsplit(char const *s, char c)
 		// 		len++;
 		// str = (char**)malloc(sizeof(char) * (++len));
 		//printf("%zu\n", ft_strchr_qt((char*)s, c));
-		str = (char**)malloc(sizeof(char) * (ft_strchr_qt((char*)s, c)));
+		if (!(str = (char**)malloc(sizeof(char) * (ft_strchr_qt((char*)s, c)))))
+			return (NULL);
 		// str[0] = "test";
 		// str[1] = "HELP";
 //---------------------------------------------------------------
 		index = 0;
-		// beg = (char*)s;
-		// end = beg;
-		while (*s != '\0')
+		beg = (char*)s;		
+		while (*s++)
 		{
-			// if (*end == c)
-			// {
-			// 	temp = (char*)malloc(sizeof(char) * (ft_ptrlen(beg, end) + 1));
-			// 	temp = ft_strcpy(beg, end);
-			// 	str[index] = temp;
-			// 	free(temp);
-			// 	index++;
-			// }
-			s++;
+			if (*s == c)
+			{
+				end = (char*)s;
+				// printf("c beg: %c\n", *beg);
+				// printf("c end: %c\n", *end);
+				// printf("p beg: %p\n", beg);
+				// printf("p end: %p\n", end);
+				//printf("(1) len: %zu\n", ft_ptrlen(beg, end));
+				temp = (char*)malloc(sizeof(char) * (ft_ptrlen(beg, end)));
+				//ft_strncpy(temp, beg, ft_ptrlen(beg, end));
+				// положить '\0' в конец
+				//printf("(2) tmp: %s\n", temp);
+				str[index++] = temp;
+				free(temp);
+				// beg = end;
+			}
 		}
 		return (str);
 	}
 	else
 		return (NULL);
-
 }
-
-
-
-		// printf("%s\n", "----------------");
-		// printf("ptr beg: %p\n", beg);
-		// printf("ptr end: %p\n", end);
-		// printf("ptr chr: %c\n", *beg);
-		// printf("ptr chr: %c\n", *end);
-
-
-
-
-
 
 
 
